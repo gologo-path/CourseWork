da96de213f12bd2a97c14b5a0ace12050fdf49ac
Volodymyr <vovkazol2000@gmail.com>
2021-05-03 16:23:25 +0300

    Minor changes in BookItem

--- src/main/java/GUI/BookItem.java
+++ src/main/java/GUI/BookItem.java
@@ -3,6 +3,7 @@ package GUI;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.uiDesigner.core.Spacer;
+import tmp.Book;
 
 import javax.swing.*;
 import java.awt.*;
@@ -17,20 +18,22 @@ public class BookItem {
     private JLabel publisher;
     private JLabel genre;
     private JLabel annotation;
-    private JButton takeButton;
+    private JButton button1;
 
     public BookItem(Book book) {
-        takeButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // TODO: 03.05.2021 Interacting with SQLManager
-            }
-        });
         name.setText(book.name);
         isbn.setText(book.isbn);
         year.setText(book.year);
         publisher.setText(book.publisher);
         genre.setText(book.genre);
         annotation.setText(book.annotation);
+    }
+
+    public void setNameButton(String name){
+        button1.setText(name);
+    }
+    public void setActionListener(ActionListener listener){
+        button1.addActionListener(listener);
     }
 
     {
@@ -91,9 +94,6 @@ public class BookItem {
         panel2.add(isbn, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer1 = new Spacer();
         panel1.add(spacer1, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
-        takeButton = new JButton();
-        takeButton.setText("Take");
-        panel1.add(takeButton, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     }
 
     /**
@@ -102,14 +102,4 @@ public class BookItem {
     public JComponent $$$getRootComponent$$$() {
         return panel1;
     }
-}
-
-// TODO: 03.05.2021 Remove Book class 
-class Book {
-    String name;
-    String isbn;
-    String year;
-    String publisher;
-    String genre;
-    String annotation;
 }
