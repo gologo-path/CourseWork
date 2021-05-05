facbd5b2d0222784cfdbfc986ec8f637fc0bfad0
Volodymyr <vovkazol2000@gmail.com>
2021-05-05 11:06:42 +0300

    Added scrollPane

--- src/main/java/GUI/SearchForm.java
+++ src/main/java/GUI/SearchForm.java
@@ -38,7 +38,6 @@ public class SearchForm {
                     throwables.printStackTrace();
                 }
                 panel1.updateUI();
-
             }
         });
         this.setGenreList();
@@ -111,9 +110,11 @@ public class SearchForm {
         defaultComboBoxModel3.addElement("Any genre");
         genre.setModel(defaultComboBoxModel3);
         panel3.add(genre, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JScrollPane scrollPane1 = new JScrollPane();
+        panel1.add(scrollPane1, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         resultPanel = new JPanel();
-        resultPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-        panel1.add(resultPanel, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        resultPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        scrollPane1.setViewportView(resultPanel);
         final Spacer spacer1 = new Spacer();
         panel1.add(spacer1, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
     }
