875b4c60f5cb244d5108ab92d53a76608973b0f9
Volodymyr <vovkazol2000@gmail.com>
2021-05-05 19:45:05 +0300

    before testing SQLBuilder

--- src/main/java/database/SQLBuilder.java
+++ src/main/java/database/SQLBuilder.java
@@ -1,17 +1,18 @@
 package database;
 
 public class SQLBuilder {
-    private String SQL;
+    private String SQL = "";
 
-    public SQLBuilder(String by, String language, String genre){
+    public SQLBuilder(String by, String language, String genre, String search){
+        SQL += "SELECT isbn, book.name AS name, year, publisher.name AS publisher, language, annotation, location FROM (book INNER JOIN publisher ON book.id_publisher = publisher.id) INNER JOIN LANGUAGE ON book.id_language ";
         if (by.equals("By name")){
-
+            SQL += "WHERE book.name LIKE '%"+search+"'";
         }else if (by.equals("By author")){
 
         }else if (by.equals("By publisher")){
-
+            SQL += "WHERE publisher.name LIKE '%"+search+"'";
         }else if (by.equals("By ISBN")){
-
+            SQL += "WHERE book.isbn = "+search;
         }
     }
 
